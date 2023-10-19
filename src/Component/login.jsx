f6cb5bb500e0a691604c734ec209a396da2ed3d3
nova.hrt <novahertanti@gmail.com>
2023-10-19 21:42:41 +0700

    progress1 revisi

--- src/Component/login.jsx
+++ src/Component/login.jsx
@@ -1,27 +1,97 @@
-import React from 'react';
-import './Login.css'; // Impor file CSS yang sesuai
+import React, { useState } from 'react';
 
-function Login() {
+function LoginForm() {
+  const [nama, setNama] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [namaError, setNamaError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNamaError('');
+    setEmailError('');
+    setPasswordError('');
+
+    if (nama.trim() === '' || nama.length > 10 || !/^[A-Za-z]*$/.test(nama)) {
+      setNamaError('Nama tidak valid.');
+    }
+
+    if (!email.includes('@')) {
+      setEmailError('Format Email tidak valid');
+    }
+
+    if (password.length < 8 || !/^(?=.*[A-Za-z])(?=.*\d)/.test(password)) {
+      setPasswordError('Password harus memiliki 8 karakter berupa huruf dan angka.');
+    }
+  };
+
   return (
-    <div className="login-container">
-      <h1>Masuk</h1>
-      <form>
-        <div className="input-container">
-          <label for="name">Nama</label>
-          <input type="text" id="name" name="name" placeholder="Masukkan Nama" />
+    <div className="login-form">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label for="nama">Nama</label>
+          <input
+            type="text"
+            name="nama"
+            id="nama"
+            value={nama}
+            onChange={(e) => setNama(e.target.value)}
+          />
+          <span id="nama-error" className="error">
+            {namaError}
+          </span>
         </div>
-        <div className="input-container">
+        <div className="form-group">
           <label for="email">Email</label>
-          <input type="email" id="email" name="email" placeholder="Masukkan Email" />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <span id="email-error" className="error">
+            {emailError}
+          </span>
         </div>
-        <div className="input-container">
+        <div className="form-group">
           <label for="password">Password</label>
-          <input type="password" id="password" name="password" placeholder="Masukkan Password" />
+          <div style={{ position: 'relative' }}>
+            <input
+              type={passwordVisible ? 'text' : 'password'}
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <i
+              className={`password-toggle far ${passwordVisible ? 'fa-eye' : 'fa-eye-slash'}`}
+              id="password-toggle"
+              onClick={togglePasswordVisibility}
+            ></i>
+          </div>
+          <span id="password-error" className="error">
+            {passwordError}
+          </span>
         </div>
-        <button type="submit">Masuk</button>
+        <button type="submit" id="login-button">
+          Login
+        </button>
       </form>
+      <br />
+      <div className="sign-up-link">
+        Belum memiliki akun? <a href="register.html">Daftar Sekarang</a>
+      </div>
     </div>
   );
 }
 
-export default Login;
+export default LoginForm;
--- src/Component/registrationform.jsx
+++ src/Component/registrationform.jsx
@@ -1,127 +1,49 @@
 import React, { useState } from 'react';
 
-function RegistrationForm() {
+const RegistrationForm = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+
   const [nameError, setNameError] = useState('');
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
   const [confirmPasswordError, setConfirmPasswordError] = useState('');
 
-  const togglePasswordVisibility = (input, setInputType) => {
-    setInputType(input.type === 'password' ? 'text' : 'password');
+  const togglePasswordVisibility = (input) => {
+    // Implement your password visibility toggle logic here
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setNameError('');
-    setEmailError('');
-    setPasswordError('');
-    setConfirmPasswordError('');
-
-    if (name.trim() === '') {
-      setNameError('Name is required.');
-    } else if (!/^[A-Za-z]+$/.test(name)) {
-      setNameError('Name should only contain letters.');
-    } else if (name.length > 10) {
-      setNameError('Name should be a maximum of 10 characters.');
-    }
-
-    if (!email.includes('@')) {
-      setEmailError('Invalid email format.');
-    }
-
-    if (password.length < 8 || !/^(?=.*[A-Za-z])(?=.*\d)/.test(password)) {
-      setPasswordError('Password must be at least 8 characters and contain letters and numbers.');
-    }
-
-    if (password !== confirmPassword) {
-      setConfirmPasswordError('Password confirmation does not match.');
-    }
+  const validateForm = () => {
+    // Implement your form validation logic here
   };
 
   return (
     <div className="registration-form">
       <h2>Registrasi</h2>
-      <form onSubmit={handleSubmit}>
+      <form>
         <div className="form-group">
-          <label for="name">Name</label>
+          <label htmlFor="name">Name</label>
           <input
             type="text"
             name="name"
             id="name"
-            required
             value={name}
             onChange={(e) => setName(e.target.value)}
+            required
           />
           <div className="error" id="name-error">
             {nameError}
           </div>
         </div>
-        <div className="form-group">
-          <label for="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <div className="error" id="email-error">
-            {emailError}
-          </div>
-        </div>
-        <div className="form-group">
-          <label for="password">Password</label>
-          <div style={{ position: 'relative' }}>
-            <input
-              type={password === 'password' ? 'text' : 'password'}
-              name="password"
-              id="password"
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <i
-              className="password-toggle far fa-eye-slash"
-              id="password-toggle"
-              onClick={() => togglePasswordVisibility(password, setPassword)}
-            ></i>
-          </div>
-          <div className="error" id="password-error">
-            {passwordError}
-          </div>
-        </div>
-        <div className="form-group">
-          <label for="confirmPassword">Confirm Password</label>
-          <div style={{ position: 'relative' }}>
-            <input
-              type={confirmPassword === 'password' ? 'text' : 'password'}
-              name="confirmPassword"
-              id="confirmPassword"
-              required
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-            <i
-              className="password-toggle far fa-eye-slash"
-              id="confirmPassword-toggle"
-              onClick={() =>
-                togglePasswordVisibility(confirmPassword, setConfirmPassword)
-              }
-            ></i>
-          </div>
-          <div className="error" id="confirmPassword-error">
-            {confirmPasswordError}
-          </div>
-        </div>
-        <button type="submit">Register</button>
+        {/* Repeat similar structure for other form fields */}
+        <button type="button" onClick={validateForm}>
+          Register
+        </button>
       </form>
     </div>
   );
-}
+};
 
 export default RegistrationForm;
