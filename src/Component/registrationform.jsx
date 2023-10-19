f99f0d0865384e720819517eac10df16a79b0d0d
nova.hrt <novahertanti@gmail.com>
2023-10-19 19:31:34 +0700

    progress 1

--- src/Component/registrationform.jsx
+++ src/Component/registrationform.jsx
@@ -1,48 +1,44 @@
 import React, { useState } from 'react';
 
 function RegistrationForm() {
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-  });
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState('');
 
-  const [errors, setErrors] = useState({});
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const validateForm = () => {
-    const newErrors = {};
-
-    if (formData.password.length > 8) {
-      newErrors.password = 'Password harus kurang dari 8 karakter';
-    }
-
-    if (!/^(?=.*[a-z])(?=.*[0-9])/.test(formData.password)) {
-      newErrors.password = 'Password harus mengandung huruf kecil dan alfanumerik';
-    }
-
-    if (formData.password !== formData.confirmPassword) {
-      newErrors.confirmPassword = 'Password dan Konfirmasi Password harus cocok';
-    }
-
-    return newErrors;
+  const togglePasswordVisibility = (input, setInputType) => {
+    setInputType(input.type === 'password' ? 'text' : 'password');
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newErrors = validateForm();
-    if (Object.keys(newErrors).length === 0) {
-      // Formulir valid, lanjutkan proses pendaftaran
-      console.log('Formulir valid:', formData);
-    } else {
-      // Formulir tidak valid, atur pesan kesalahan
-      setErrors(newErrors);
+    setNameError('');
+    setEmailError('');
+    setPasswordError('');
+    setConfirmPasswordError('');
+
+    if (name.trim() === '') {
+      setNameError('Name is required.');
+    } else if (!/^[A-Za-z]+$/.test(name)) {
+      setNameError('Name should only contain letters.');
+    } else if (name.length > 10) {
+      setNameError('Name should be a maximum of 10 characters.');
+    }
+
+    if (!email.includes('@')) {
+      setEmailError('Invalid email format.');
+    }
+
+    if (password.length < 8 || !/^(?=.*[A-Za-z])(?=.*\d)/.test(password)) {
+      setPasswordError('Password must be at least 8 characters and contain letters and numbers.');
+    }
+
+    if (password !== confirmPassword) {
+      setConfirmPasswordError('Password confirmation does not match.');
     }
   };
 
@@ -51,67 +47,77 @@ function RegistrationForm() {
       <h2>Registrasi</h2>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <label for="firstName">First Name</label>
-          <br />
+          <label for="name">Name</label>
           <input
             type="text"
-            name="firstName"
-            id="firstName"
-            value={formData.firstName}
-            onChange={handleChange}
+            name="name"
+            id="name"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
-          <div className="error" id="firstName-error"></div>
-        </div>
-        <div className="form-group">
-          <label for="lastName">Last Name</label>
-          <br />
-          <input
-            type="text"
-            name="lastName"
-            id="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-          />
-          <div className="error" id="lastName-error"></div>
+          <div className="error" id="name-error">
+            {nameError}
+          </div>
         </div>
         <div className="form-group">
           <label for="email">Email</label>
-          <br />
           <input
             type="email"
             name="email"
             id="email"
-            value={formData.email}
-            onChange={handleChange}
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
-          <div className="error" id="email-error"></div>
+          <div className="error" id="email-error">
+            {emailError}
+          </div>
         </div>
         <div className="form-group">
           <label for="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-          {errors.password && <div className="error">{errors.password}</div>}
+          <div style={{ position: 'relative' }}>
+            <input
+              type={password === 'password' ? 'text' : 'password'}
+              name="password"
+              id="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <i
+              className="password-toggle far fa-eye-slash"
+              id="password-toggle"
+              onClick={() => togglePasswordVisibility(password, setPassword)}
+            ></i>
+          </div>
+          <div className="error" id="password-error">
+            {passwordError}
+          </div>
         </div>
         <div className="form-group">
           <label for="confirmPassword">Confirm Password</label>
-          <br />
-          <input
-            type="password"
-            name="confirmPassword"
-            id="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-          />
-          {errors.confirmPassword && (
-            <div className="error">{errors.confirmPassword}</div>
-          )}
+          <div style={{ position: 'relative' }}>
+            <input
+              type={confirmPassword === 'password' ? 'text' : 'password'}
+              name="confirmPassword"
+              id="confirmPassword"
+              required
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <i
+              className="password-toggle far fa-eye-slash"
+              id="confirmPassword-toggle"
+              onClick={() =>
+                togglePasswordVisibility(confirmPassword, setConfirmPassword)
+              }
+            ></i>
+          </div>
+          <div className="error" id="confirmPassword-error">
+            {confirmPasswordError}
+          </div>
         </div>
-        {/* Tambahkan tombol submit */}
         <button type="submit">Register</button>
       </form>
     </div>
